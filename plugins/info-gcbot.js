d83303524e6f41a953c8ed54d9a3b7eb91cd130c
SaepEp <105292856+SaepEp@users.noreply.github.com>
2022-11-06 22:53:34 +0700

    Add files via upload

--- plugins/info-gcbot.js
+++ plugins/info-gcbot.js
@@ -1,14 +1,10 @@
-import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let pp = await (await fetch(`https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group`))
+let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.wm
 let str = `
 ✧─────[ *Group Official* ]─────✧
 🍀 Group 1 :
-https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL
-
-🍀 Youtube :
-https://youtu.be/pwLZpdfO8AU
+https://linktr.ee/ohlxbot
 ✧──────────···──────────✧
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, [['⋮☰ MENU','.menu']],m)
