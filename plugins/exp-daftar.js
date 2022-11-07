a88dd9801620c231a10e0ee6695267bfd56096ff
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-11-07 13:16:24 +0700

    exp-daftar.js

--- plugins/exp-daftar.js
+++ plugins/exp-daftar.js
@@ -9,55 +9,55 @@ let handler = async function (m, { text, usedPrefix, command }) {
 	let namae = conn.getName(m.sender)
 	const sections = [
 	{
-	title: "Pilih Umur Kamu Disini !",
+	title: "📆Select Your Age Here !",
 	rows: [
-	    {title: "Random Years", rowId: '.daftar ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])}
+	    {title: "📛Random Years", rowId: '.daftar ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])}
 	]
     },
     {
-	title: "O L D",
+	title: "🎀O L D",
 	rows: [
-	    {title: "30 Years", rowId: '.daftar ' + namae + '.30 '},
-	    {title: "29 Years", rowId: '.daftar ' + namae + '.29 '},
-	    {title: "28 Years", rowId: '.daftar ' + namae + '.28 '},
-	{title: "27 Years", rowId: '.daftar ' + namae + '.27 '},
-	{title: "26 Years", rowId: '.daftar ' + namae + '.26 '},
-	{title: "25 Years", rowId: '.daftar ' + namae + '.25 '},
-	{title: "24 Years", rowId: '.daftar ' + namae + '.24 '},
-	{title: "23 Years", rowId: '.daftar ' + namae + '.23 '},
-	{title: "22 Years", rowId: '.daftar ' + namae + '.22 '},
-	{title: "21 Years", rowId: '.daftar ' + namae + '.21 '}
+	    {title: "🌸30• Years ʚĭɞ", rowId: '.daftar ' + namae + '.30 '},
+	    {title: "🎐29• Years ʚĭɞ", rowId: '.daftar ' + namae + '.29 '},
+	    {title: "🌸28• Years ʚĭɞ", rowId: '.daftar ' + namae + '.28 '},
+	{title: "🎐27• Years ʚĭɞ", rowId: '.daftar ' + namae + '.27 '},
+	{title: "🌸26• Years ʚĭɞ", rowId: '.daftar ' + namae + '.26 '},
+	{title: "🎐25• Years ʚĭɞ", rowId: '.daftar ' + namae + '.25 '},
+	{title: "🌸24• Years ʚĭɞ", rowId: '.daftar ' + namae + '.24 '},
+	{title: "🎐23• Years ʚĭɞ", rowId: '.daftar ' + namae + '.23 '},
+	{title: "🌸22• Years ʚĭɞ", rowId: '.daftar ' + namae + '.22 '},
+	{title: "🎐21• Years ʚĭɞ", rowId: '.daftar ' + namae + '.21 '}
 	]
     },
     {
-	title: "Y O U N G",
+	title: "🎀Y O U N G",
 	rows: [
-	    {title: "20 Years", rowId: '.daftar ' + namae + '.20 '},
-	    {title: "19 Years", rowId: '.daftar ' + namae + '.19 '},
-	    {title: "18 Years", rowId: '.daftar ' + namae + '.18 '},
-	{title: "17 Years", rowId: '.daftar ' + namae + '.17 '},
-	{title: "16 Years", rowId: '.daftar ' + namae + '.16 '},
-	{title: "15 Years", rowId: '.daftar ' + namae + '.15 '},
-	{title: "14 Years", rowId: '.daftar ' + namae + '.14 '},
-	{title: "13 Years", rowId: '.daftar ' + namae + '.13 '},
-	{title: "12 Years", rowId: '.daftar ' + namae + '.12 '},
-	{title: "11 Years", rowId: '.daftar ' + namae + '.11 '},
-	{title: "10 Years", rowId: '.daftar ' + namae + '.10 '},
-	{title: "9 Years", rowId: '.daftar ' + namae + '.9 '}
+	    {title: "🌸20• Years ʚĭɞ", rowId: '.daftar ' + namae + '.20 '},
+	    {title: "🎐19• Years ʚĭɞ", rowId: '.daftar ' + namae + '.19 '},
+	    {title: "🌸18• Years ʚĭɞ", rowId: '.daftar ' + namae + '.18 '},
+	{title: "🎐17• Years ʚĭɞ", rowId: '.daftar ' + namae + '.17 '},
+	{title: "🌸16• Years ʚĭɞ", rowId: '.daftar ' + namae + '.16 '},
+	{title: "🎐15• Years ʚĭɞ", rowId: '.daftar ' + namae + '.15 '},
+	{title: "🌸14• Years ʚĭɞ", rowId: '.daftar ' + namae + '.14 '},
+	{title: "🎐13• Years ʚĭɞ", rowId: '.daftar ' + namae + '.13 '},
+	{title: "🌸12• Years ʚĭɞ", rowId: '.daftar ' + namae + '.12 '},
+	{title: "🎐11• Years ʚĭɞ", rowId: '.daftar ' + namae + '.11 '},
+	{title: "🌸10• Years ʚĭɞ", rowId: '.daftar ' + namae + '.10 '},
+	{title: "🎐9• Years ʚĭɞ", rowId: '.daftar ' + namae + '.9 '}
 	]
     },
 ]
 
 const listMessage = {
-  text: `│›Silahkan Pilih Umur Anda Di Bawah Ini...`,
-  footer: `┗ *Nama kamu:* ${conn.getName(m.sender)}\n<❔>Ingin nama Kostum? jenis *${usedPrefix + command} nama lu.umur lu*`,
-  title: "▢- - - - - DAFTAR - - - - -",
-  buttonText: "Click Here !",
+  text: `│›Please select your age at the bottom button...`,
+  footer: `┗ *ʏᴏᴜʀ ɴᴀᴍᴇ:* ${conn.getName(m.sender)}\n<❔>  BOT PRIVATE\n⚘ ᴄʀᴇᴀᴛᴇ ʙʏ ᴹᴿ᭄ King Of Bear ×፝֟͜×`,
+  title: "▢━━━━「 Registration 」━━━━▢",
+  buttonText: "ഒ Register ഒ",
   sections
 }
 
   let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `[💬] Kamu sudah terdaftar\nMau daftar ulang? *${usedPrefix}unreg <SERIAL NUMBER>*`
+  if (user.registered === true) throw `📮Kamu Sudah ter daftar di database, Apa kamu ingin mendaftar ulang? *${usedPrefix}unreg <SERIAL NUMBER>*`
   if (!Reg.test(text)) return conn.sendMessage(m.chat, listMessage, { quoted: m })
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
@@ -72,37 +72,39 @@ const listMessage = {
   let sn = createHash('md5').update(m.sender).digest('hex')
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender
   let cap = `
-┏─• *ᴜsᴇʀs*
-│▸ *sᴛᴀᴛᴜs:* SUKSES TERDAFTAR ☑️
+  ━━━━ 「 *Successful Registration* 」━━━
+  .
+╭━━「 *ᴜsᴇʀs* 」
+│▸ *sᴛᴀᴛᴜs:* ☑️ sᴜᴄᴄᴇssғᴜʟ
 │▸ *ɴᴀᴍᴇ:* ${name}
 │▸ *ᴀɢᴇ:* ${age} ʏᴇᴀʀs
 │▸ *sɴ:* ${sn}
-┗────···
+╰═┅═━––––––๑
 
-ᴅᴀᴛᴀ ᴜsᴇʀ ʏᴀɴɢ ᴛᴇʀsɪᴍᴘᴀɴ ᴅɪᴅᴀᴛᴀʙᴀsᴇ ʙᴏᴛ, ᴅɪᴊᴀᴍɪɴ ᴀᴍᴀɴ ᴛᴀɴᴘᴀ ᴛᴇʀsʜᴀʀᴇ (. ❛ ᴗ ❛.)
+ꜱᴜʙꜱᴄʀɪʙᴇ ʏᴛ: ᴹᴿ᭄ King Of Bear ×፝֟͜×
 `
   let buttonMessage= {
-'document':{'url':sgc},
+'document':{'url':'https://youtu.be/pwLZpdfO8AU'},
 'mimetype':global.ddocx,
-'fileName':'- - - - - ʀᴇɢɪsᴛᴇʀ - - - - -',
+'fileName':'▢━━━━「 Registration 」━━━━▢',
 'fileLength':fsizedoc,
 'pageCount':fpagedoc,
 'contextInfo':{
 'forwardingScore':555,
 'isForwarded':true,
 'externalAdReply':{
-'mediaUrl':global.sig,
+'mediaUrl':'https://youtu.be/pwLZpdfO8AU',
 'mediaType':2,
 'previewType':'pdf',
 'title':global.titlebot,
 'body':global.titlebot,
-'thumbnail':await(await fetch('https://telegra.ph/file/4a7e5f18efaadec18a7a0.jpg')).buffer(),
-'sourceUrl':sgc}},
+'thumbnail':await(await fetch('https://telegra.ph/file/6ab4daac226292a112540.jpg')).buffer(),
+'sourceUrl':'https://youtu.be/pwLZpdfO8AU'}},
 'caption':cap,
 'footer':botdate,
 'buttons':[
-{'buttonId':'.menu','buttonText':{'displayText':'ᴍᴇɴᴜ'},'type':1},
-{'buttonId':'.donasi','buttonText':{'displayText':'ᴅᴏɴᴀsɪ'},'type':1}
+{'buttonId':'.menu','buttonText':{'displayText':'▣ Mҽɳυ ▣'},'type':1},
+{'buttonId':'.salken','buttonText':{'displayText':'🎈Hαʅʅσ'},'type':1}
 ],
 'headerType':6}
     await conn.sendMessage(m.chat,buttonMessage, { quoted:m})
