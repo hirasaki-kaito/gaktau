efad89555dc587bf49f82d7fd432599e331ead07
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-11-08 23:16:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,15 +44,15 @@ global.ppulsa2 = '088293499014'
 global.psaweria = '-'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6283848389595'
-global.nomorown = '6282137288121'
+global.nomorbot = '6287776985031'
+global.nomorown = '6288293499014'
 global.namebot = 'kzbotz'
 global.nameown = 'kz'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282137288121', 'kz', 'true']
+  ['6288293499014', 'kz', 'true']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
