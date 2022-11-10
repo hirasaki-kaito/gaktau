d09238bbfc9f28b6070e8cabfcf72b006a35ce90
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-11-10 17:48:58 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,17 +20,17 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : Kz
-${htjava} *✉️ Nama RL* : Rehan
+${htjava} *💌 Nama* : kazuta
+${htjava} *✉️ Nama RL* : zaaa
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
-${htjava} *🎨 Umur* : 17
+${htjava} *🎨 Umur* : 16
 ${htjava} *🧮 Kelas* : PRIVATE
-${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP
+${htjava} *🧩 Hobby* : Main Moba legenda
 ${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Jawa Barat, Bogor
-${htjava} *❤️ Waifu* : Raphtalia
+${htjava} *🗺️ Tinggal* : Jatim
+${htjava} *❤️ Waifu* : Elaina
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
 •·––––––––––––––––––––––––––·•
