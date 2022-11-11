b82172f2c53877078fef0ee5ce86db734265a6d5
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-11-11 21:54:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
       
 
 /*============== INFO OWNER ==============*/
-global.sig = 'https://instagram.com/rhandrqz_'
+global.sig = 'https://instagram.com/'_'
 global.syt = 'https://youtube.com/channel/UC28pUSJ1MyZhqGL_lamNQRw'
 global.sgh = 'https://github.com/Kzdika'
 global.sgc = 'https://chat.whatsapp.com/ECmDXCJBCFEEtr3J2RvtTg'
