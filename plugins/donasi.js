167a03643502237e48c8b37ffcf8a3920d6c817e
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-11-12 07:33:29 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,13 +2,13 @@ import fs from 'fs';
 let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
-    let img2 = `https://telegra.ph/file/a5eb5169ab1330a537e0b.jpg`
+    let img2 = `https://telegra.ph/file/2c9ecb22818cf0e283046.jpg`
     return conn.sendButton(m.chat, 'Donasi Pm Owner aja.\n❏  :v', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
-                mediaUrl: "https://instagram.com/saep.02",
+                mediaUrl: "https://instagram.com/rhandrqz_",
                 mediaType: "VIDEO",
-                title: 'Donasi To NexEp ',
+                title: 'Donasi To Kzbotz ',
                 body: bottime,
                 previewType: 0,
                 thumbnail: img
