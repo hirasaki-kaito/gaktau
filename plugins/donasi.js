7a569419c8ff379c4ce714d4bef1ba0212f6990f
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-11-13 04:19:13 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
-    let img2 = `https://telegra.ph/file/2b2cbbd0689528d384122.jpg'
+    let img2 = `https://telegra.ph/file/284c5c84983f4b5ee1eb2.jpg'
     return conn.sendButton(m.chat, 'Donasi Pm Owner aja.\n❏  :v', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
