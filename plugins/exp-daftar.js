2298f7ac222e7c39bf4b8adf6e61e8f131bb31e0
Kzdika <115381989+Kzdika@users.noreply.github.com>
2022-11-16 05:02:55 +0700

    Update exp-daftar.js

--- plugins/exp-daftar.js
+++ plugins/exp-daftar.js
@@ -92,13 +92,13 @@ const listMessage = {
 'forwardingScore':555,
 'isForwarded':true,
 'externalAdReply':{
-'mediaUrl':'https://instagram.com/rhandrqz_',
+'mediaUrl':'_',
 'mediaType':2,
 'previewType':'pdf',
 'title':global.titlebot,
 'body':global.titlebot,
 'thumbnail':await(await fetch('https://telegra.ph/file/93dd3203b2bc4cd262ee7.jpg')).buffer(),
-'sourceUrl':'https://instagram.com/rhandrqz_'}},
+'sourceUrl':'_'}},
 'caption':cap,
 'footer':botdate,
 'buttons':[
